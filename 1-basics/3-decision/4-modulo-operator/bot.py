d1f17dfef8c1226e4feaf80682c23538a0ac4717
DiogoRHJMatos <4higid44@solent.ac.uk>
2019-10-03 10:01:47 +0000

    amend

---
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -0,0 +1,9 @@
+print("Please enter a whole number.")
+
+num = int(input())
+
+if((num % 2) == 0):
+    print("The number " + str(number) + " is an even number")
+
+else:
+    print("The number " + str(number) + " is an odd number")
