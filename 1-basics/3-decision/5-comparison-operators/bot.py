53f6d4739678c18b1ec18e1822b52ff8671a50ff
DiogoRHJMatos <4higid44@solent.ac.uk>
2019-10-03 10:07:38 +0000

    amend

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,14 @@
+print("Please enter first number")
+first = int(input())
+
+print("Please enter second number")
+second = int(input())
+
+if(first > second):
+    print("the first is the biggest")
+
+elif(first < second):
+    print("the second is the biggest")
+
+else:
+    print("they are equal")
